50b1caad8b8ad9581bc6841cf1ee56c068003231
Yuki MIZUNO <mizuyuu0904@gmail.com>
2015-04-22 20:37:35 +0900

    Change paths of HLS files

--- nginx-hls.sh
+++ nginx-hls.sh
@@ -10,7 +10,6 @@ FPS=24
 SEGLEN=15 # length of one segment in seconds
 DELSEGS=true # delete already broadcasted segments
 NUMSEGS=10 # segments count
-NAME=live
 
 # RAM disk settings
 TMPSIZE=128 # RAM disk size in MB
@@ -32,7 +31,8 @@ TMPDIR=$(mktemp -d)
 sudo mv ${NGINXDIR}/* ${TMPDIR}/
 sudo mount -t tmpfs -o size=${TMPSIZE}m /dev/shm ${NGINXDIR}
 sudo cp -r ${PWD}/www/* ${NGINXDIR}/
+mkdir -p ${NGINXDIR}/live
 trap cleanup INT TERM
 vlc -I dummy v4l2://${WEBCAM}:chroma="H264":width=${WIDTH}:height=${HEIGHT}:fps=${FPS} vlc://quit \
   --sout="#standard{access=livehttp{seglen=${SEGLEN},delsegs=${DELSEGS},numsegs=${NUMSEGS}, \
-  index=${NGINXDIR}/${NAME}.m3u8,index-url=/${NAME}-########.ts},mux=ts{use-key-frames},dst=${NGINXDIR}/${NAME}-########.ts}"
+  index=${NGINXDIR}/live/index.m3u8,index-url=/live/segment-########.ts},mux=ts{use-key-frames},dst=${NGINXDIR}/live/segment-########.ts}"
